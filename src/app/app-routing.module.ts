c53a23e54b8cb45a658a65d19c47656e8ef3fe13
Gayduk Dmitry <gaydukdim@yandex.ru>
2019-01-12 11:08:03 +0300

    add 3 components with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GamesComponent }  from './games/games.component';
+import { DevelopersComponent }  from './developers/developers.component';
+import { PublishersComponent }  from './publishers/publishers.component';
+
+const routes: Routes = [
+  {
+    path: 'games',
+    component: GamesComponent
+  },
+  {
+    path: 'developers',
+    component: DevelopersComponent
+  },
+  {
+    path: 'publishers',
+    component: PublishersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { PublishersComponent } from './publishers/publishers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
+    DevelopersComponent,
+    PublishersComponent
   ],
   imports: [
     BrowserModule,
